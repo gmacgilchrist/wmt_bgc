867a59d66bcd61b72d01bec3cb19c9539161e0a8
gmacgilchrist <graemem@princeton.edu>
2020-08-31 18:28:50 -0400

    first commit in a while

---
+++ archive/calc_wmt_old200507.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# Series of functions pertaining to calculating watermass transformation
+import xarray as xr
+import numpy as np
+from xhistogram.xarray import histogram
+#from xarrayutils import vertical_coordinates as vc
+
+def calc_G(l,dl,l_i_vals,area,plot=False):
+    '''Mass transport across contours of [l].
+    l : intensive variable, to evaluate transport across
+    dl : summed tendency of (vertically integrated) l due to diffusive processes;
+        e.g. heat tendency if l if temperature
+    l_i_vals : interface values of l
+    area : 2d distribution of horizontal area (dx*dy)'''
+    
+    # Get the spacing of contours of l
+    delta_l_vals = np.diff(l_i_vals)
+    # xhistogram has some curiousities around nan-values, so exclude them
+    nanmask = np.isnan(l)
+    # Integrate [dl] within layers of [l] and divide by [delta_l]
+    G = histogram(l.where(~nanmask),bins=[l_i_vals],weights=(dl*area).where(~nanmask),dim=['xh','yh','zl'],block_size=1)/delta_l_vals
+    
+    if plot:
+        G.plot()
+    return G
+
+def calc_E(c,l,dl,l_i_vals,area,plot=False):
+    '''Transport of tracer [c] mass across contours of [l].
+    c : tracer for which to evaluate transport (units of tracer mass per kg)
+    l : intensive variable, to evaluate transport across
+    dl : summed tendency of (vertically integrated) l due to diffusive processes;
+        e.g. heat tendency if l if temperature
+    l_i_vals : interface values of l
+    area : 2d distribution of horizontal area (dx*dy)'''
+    
+    # Get the spacing of contours of l
+    delta_l_vals = np.diff(l_i_vals)
+    # xhistogram has some curiousities around nan-values, so exclude them
+    nanmask = np.isnan(l)
+    # Integrate [c*dl] within layers of [l] and divide by [delta_l]
+    E = histogram(l.where(~nanmask),bins=[l_i_vals],weights=(c*dl*area).where(~nanmask),dim=['xh','yh','zl'],block_size=1)/delta_l_vals
+    if plot:
+        E.plot()
+    return E
+
+def calc_P(p,l,l_i_vals,area,greaterthan=True):
+    '''Integration of quantity [p] across volume with [l] greater than *mid-point* 
+    values between layer interfaces [l_i_vals] (i.e. the center layer values).
+    This is done so that the output of calc_P aligns with that of calc_G and calc_E,
+    when given the same l_i_vals.
+    
+    *** IMPORTANT *** 
+    Limits of l_i_vals should be such that their mid-points span the full range of l 
+    *****************
+    
+    p : quantity to be integrated (e.g. tendencies of independent tracer [c])
+    l : intensive variable, to define volume boundary
+    l_i_vals : interface values of l 
+        (integral is done for l[i] >= 0.5*(l_i_vals[i]+l_i_vals[i+1]))
+    area : 2d distribution of horizontal area (dx*dy)
+    greaterthan : boolean, True if the integral should be over contours 
+        greater than each layer interface. False for less than.'''
+    
+    # Get the mid-points of the layers (as defined by their interfaces)
+    l_l_vals = 0.5*(l_i_vals[:-1]+l_i_vals[1:])
+    # xhistogram has some curiousities around nan-values, so exclude them
+    nanmask = np.isnan(l)
+    # Integrate p between each layer mid-point
+    P_l = histogram(l.where(~nanmask),bins=[l_l_vals],weights=(p*area).where(~nanmask),dim=['xh','yh','zl'],block_size=1)
+    # Cumulatively sum each layer (concatenate zeros at start), and take away from total sum to reverse the order of the summation
+    # i.e. this the cumulative sum of integrated [p] from largest to smallest [l_l]
+    # Concatenate zeroes
+    # At the same time, reassign the coordinates to align with the layer mid-points
+#     P = xr.concat([P_l.sum(l.name+'_bin'),(P_l.sum(l.name+'_bin')-P_l.cumsum(l.name+'_bin'))],
+#                   dim=l.name+'_bin').assign_coords({l.name+'_bin':l_l_vals})
+    P_l_cumsum = xr.concat([xr.zeros_like(P_l.isel({l.name+'_bin':0})),P_l.cumsum(l.name+'_bin')],dim=l.name+'_bin')
+    if greaterthan:
+        P = (P_l.sum(l.name+'_bin')-P_l_cumsum).assign_coords({l.name+'_bin':l_l_vals})
+    else:
+        P = P_l_cumsum.assign_coords({l.name+'_bin':l_l_vals})
+                                                                          
+    return P
+
+def calc_E_old(ds,l,l_i_vals,dl,c,area,z='depth',xdim='xh',ydim='yh',zldim='zl',zidim='zi',binning=None):
+    '''Evaluation of the transport of tracer [c] across contours of tracer [l]
+    l and c should be given as strings and both be contained in Dataset ds
+    dl corresponds to the diffusive time tendency of tracer *content* of l,
+    i.e. the tendency of rho*l*h where h is layer thickness, and rho is in situ
+    density, thus in units of [kg m^-3]*[l tracer unit]*[m]*[s-1].
+    Then
+        E(l') = \frac{1}{\Delta l}\iint_{l=l'} c*dl dA
+    This uses the fact that the tracer content tendency is equal to the 
+    rho * tracer tendency integrated within a layer (which is formulation for
+    wmt in Groeskamp et al, 2019).'''
+    
+    E = xr.Dataset()
+    
+    # Calculate tracer at layer centres from interface values
+    l_l_vals = 0.5*(l_i_vals[:-1]+l_i_vals[1:])
+    # Calculate delta_tracer for each layer
+    delta_l_vals = np.diff(l_i_vals)
+    # Place in DataArrays
+    l_i=xr.DataArray(l_i_vals,dims=['l_i'],coords={'l_i':l_i_vals})
+    l_l=xr.DataArray(l_l_vals,dims=['l_l'],coords={'l_l':l_l_vals})
+    delta_l = xr.DataArray(delta_l_vals,dims=['l_l'],coords={'l_l':l_l_vals})
+
+    # Calculate cdl
+    cdl = ds[c]*dl
+
+    # Bin into layers of l, and sum up across space
+    if binning is None:
+        work = (cdl*area).sum(dim=[xdim,ydim])
+        work = work.rename({vertc:'l_l'})
+    if binning=='xhistogram':
+        nanmask = np.isnan(cdl)
+        work = histogram(
+                    ds[l].where(~nanmask), 
+                    bins=[l_i.values], 
+                    dim=['xh','yh','zl','time'], 
+                    weights=(cdl*area).where(~nanmask),
+                    block_size=None
+                    )
+        work = work.rename({l+'_bin':'l_l'})
+    if binning=='busecke':
+        l_depth_i = vc.linear_interpolation_regrid(ds[z+'_l'], ds[l], l_i, z_bounds=ds[z+'_i'], target_value_dim='l_i', z_bounds_dim=zidim, z_dim=zldim)
+        cdl_remapped = conservative_remap(cdl,z_bnds_source=ds[z+'_i'], z_bnds_target=l_depth_i,
+                                           z_dim=zldim, z_bnd_dim=zidim, z_bnd_dim_target='regridded', mask=True)
+        work = (cdl_remapped*area).sum(dim=[xdim,ydim])
+        work = work.rename({'remapped':'l_l'})
+        E['l_depth_i'] = l_depth_i
+    
+    E['E'] = work/delta_l
+    E['dE']= E['E'].interp(l_l=l_i,method='linear').diff('l_i').assign_coords(l_i=l_l.values).drop('l_l').rename({'l_i':'l_l'})
+    
+    return E
--- calc_wmt.py
+++ calc_wmt.py
@@ -3,133 +3,211 @@
 import xarray as xr
 import numpy as np
 from xhistogram.xarray import histogram
-#from xarrayutils import vertical_coordinates as vc
-
-def calc_G(l,dl,l_i_vals,area,plot=False):
-    '''Mass transport across contours of [l].
-    l : intensive variable, to evaluate transport across
-    dl : summed tendency of (vertically integrated) l due to diffusive processes;
-        e.g. heat tendency if l if temperature
-    l_i_vals : interface values of l
-    area : 2d distribution of horizontal area (dx*dy)'''
+    
+def calc_E(l,dl,l_i_vals,c=None,weight=None,dims=['xh','yh','zl']):
+    '''Transport of mass or tracer across contours of [l], due to processes [dl].
+    l : xr.DataArray;
+        Intensive variable (e.g. temp) that defines layers across which transport
+        will be determined.
+    dl : xr.DataArray;
+        Tendency of l due to (individual or total sum of) diffusive processes,
+        e.g. heat tendencies
+        Multiple processes are included as Dataarrays inside a dataset.
+    l_i_vals : ndarray or xr.DataArray;
+        Interface values of l; transport will be across midpoints of these interfaces
+    c : None or scalar or xr.DataArray
+        If included, evaluating transport of tracer as opposed to volume
+        A scalar could be density, to get mass rather than volume transport
+    weight : xr.DataArray;
+        Distribution of weights by which to multiply [dl] to remove spatial
+        dimension of units. E.g. heat flux in Wm-2 should be multiplied by the area
+        of the grid cell to recover total heating.
+    dims : list of string;
+        Dimensions along which to perform histogram.'''
+    
+    import xarray as xr
+    import numpy as np
+    from xhistogram.xarray import histogram
     
     # Get the spacing of contours of l
     delta_l_vals = np.diff(l_i_vals)
     # xhistogram has some curiousities around nan-values, so exclude them
     nanmask = np.isnan(l)
-    # Integrate [dl] within layers of [l] and divide by [delta_l]
-    G = histogram(l.where(~nanmask),bins=[l_i_vals],weights=(dl*area).where(~nanmask),dim=['xh','yh','zl'],block_size=1)/delta_l_vals
     
-    if plot:
-        G.plot()
-    return G
-
-def calc_E(c,l,dl,l_i_vals,area,plot=False):
-    '''Transport of tracer [c] mass across contours of [l].
-    c : tracer for which to evaluate transport (units of tracer mass per kg)
-    l : intensive variable, to evaluate transport across
-    dl : summed tendency of (vertically integrated) l due to diffusive processes;
-        e.g. heat tendency if l if temperature
-    l_i_vals : interface values of l
-    area : 2d distribution of horizontal area (dx*dy)'''
+    # Specify what should be integrated
+    # (this is rather clunky with if statements, but is meant to avoid loading
+    # unnecessary variables, such as dummy [c]'s or [weight]'s)
+    if c==None:
+        if weight is None:
+            weights = dl
+        else:
+            weights = dl*weight
+    else:
+        if weight is None:
+            weights = c*dl
+        else:
+            weights = c*dl*weight
+            
+    # Integrate [weights] within layers of [l] and divide by [delta_l]
+    E = histogram(l.where(~nanmask),
+                  bins=[l_i_vals],
+                  weights=weights.where(~nanmask),
+                  dim=dims,
+                  block_size=1)/delta_l_vals
     
-    # Get the spacing of contours of l
-    delta_l_vals = np.diff(l_i_vals)
-    # xhistogram has some curiousities around nan-values, so exclude them
-    nanmask = np.isnan(l)
-    # Integrate [c*dl] within layers of [l] and divide by [delta_l]
-    E = histogram(l.where(~nanmask),bins=[l_i_vals],weights=(c*dl*area).where(~nanmask),dim=['xh','yh','zl'],block_size=1)/delta_l_vals
-    if plot:
-        E.plot()
     return E
 
-def calc_P(p,l,l_i_vals,area,greaterthan=True):
-    '''Integration of quantity [p] across volume with [l] greater than *mid-point* 
-    values between layer interfaces [l_i_vals] (i.e. the center layer values).
-    This is done so that the output of calc_P aligns with that of calc_G and calc_E,
-    when given the same l_i_vals.
+def calc_E_wrapper(l,dls,l_i_vals,c=None,weight=None,dims=['xh','yh','zl']):
+    '''Wrapper for calc_E to allow evaluation of contributions from different processes,
+    each represented as a DataArray in the Dataset [dls].
+    l : xr.DataArray;
+        Intensive variable (e.g. temp) that defines layers across which transport
+        will be determined.
+    dls : xr.Dataset;
+        Tendencies of l due to differnt diffusive processes,
+        e.g. horizontal and vertical diffusion tendencies
+        Multiple processes are included as Dataarrays inside a dataset.
+    l_i_vals : ndarray or xr.DataArray;
+        Interface values of l; transport will be across midpoints of these interfaces
+    c : None or scalar or xr.DataArray
+        If included, evaluating transport of tracer as opposed to volume
+        A scalar could be density, to get mass rather than volume transport
+    weight : xr.DataArray;
+        Distribution of weights by which to multiply [dl] to remove spatial
+        dimension of units. E.g. heat flux in Wm-2 should be multiplied by the area
+        of the grid cell to recover total heating.
+    dims : list of string;
+        Dimensions along which to perform histogram.'''
+    
+    # Set up dataset for contributions to E
+    E = xr.Dataset()
+    for var in dls.data_vars:
+        dl = dls[var]
+        E[var] = calc_E(l,dl,l_i_vals,c=c,weight=weight,dims=dims)
+        
+    return E
+
+def calc_volumetric_cumsum(l,p,l_l_vals,weight=None,dims=['xh','yh','zl'],greaterthan=True,):
+    '''Integration of quantity [p] across volume with [l] greater than layer defined
+    by contours l_l_vals.
     
     *** IMPORTANT *** 
-    Limits of l_i_vals should be such that their mid-points span the full range of l 
+    Limits of l_l_vals must span the full range of l.
     *****************
     
-    p : quantity to be integrated (e.g. tendencies of independent tracer [c])
-    l : intensive variable, to define volume boundary
-    l_i_vals : interface values of l 
-        (integral is done for l[i] >= 0.5*(l_i_vals[i]+l_i_vals[i+1]))
-    area : 2d distribution of horizontal area (dx*dy)
-    greaterthan : boolean, True if the integral should be over contours 
-        greater than each layer interface. False for less than.'''
+    l : xr.DataArray;
+        Intensive variable, to define volume boundary
+    p : xr.DataArray;
+        Quantity to be integrated (e.g. thickness)
+    l_l_vals : ndarray or xr.DataArray
+        Values of the contours for which the integration will be calculated
+        *** This is currently set up such that the layer values are increasing ***
+        *** I expect it can be generalized, but have not explored this yet     ***
+    weight : Distribution of weights by which to multiply [p] to remove spatial
+        dimension of units. E.g. thickness in units 'm' should be multiplied by the area
+        of the grid cell to recover total volume.
+    greaterthan : boolean;
+        True if the integral should be over contours 
+        greater than each contour. False for less than.'''
     
-    # Get the mid-points of the layers (as defined by their interfaces)
-    l_l_vals = 0.5*(l_i_vals[:-1]+l_i_vals[1:])
     # xhistogram has some curiousities around nan-values, so exclude them
     nanmask = np.isnan(l)
-    # Integrate p between each layer mid-point
-    P_l = histogram(l.where(~nanmask),bins=[l_l_vals],weights=(p*area).where(~nanmask),dim=['xh','yh','zl'],block_size=1)
+    
+    # Specify what should be integrated
+    if weight is None:
+        weights = p
+    else:
+        weights = p*weight
+        
+    # Integrate p between each contour layer
+    P_l = histogram(l.where(~nanmask),bins=[l_l_vals],weights=weights.where(~nanmask),dim=dims,block_size=1)
     # Cumulatively sum each layer (concatenate zeros at start), and take away from total sum to reverse the order of the summation
     # i.e. this the cumulative sum of integrated [p] from largest to smallest [l_l]
-    # Concatenate zeroes
-    # At the same time, reassign the coordinates to align with the layer mid-points
-#     P = xr.concat([P_l.sum(l.name+'_bin'),(P_l.sum(l.name+'_bin')-P_l.cumsum(l.name+'_bin'))],
-#                   dim=l.name+'_bin').assign_coords({l.name+'_bin':l_l_vals})
     P_l_cumsum = xr.concat([xr.zeros_like(P_l.isel({l.name+'_bin':0})),P_l.cumsum(l.name+'_bin')],dim=l.name+'_bin')
     if greaterthan:
+        # Take cumulative sum away from total sum to reverse the order
+        # Assign coordinates to match the contours (rather than the interfaces)
         P = (P_l.sum(l.name+'_bin')-P_l_cumsum).assign_coords({l.name+'_bin':l_l_vals})
     else:
         P = P_l_cumsum.assign_coords({l.name+'_bin':l_l_vals})
                                                                           
     return P
 
-def calc_E_old(ds,l,l_i_vals,dl,c,area,z='depth',xdim='xh',ydim='yh',zldim='zl',zidim='zi',binning=None):
-    '''Evaluation of the transport of tracer [c] across contours of tracer [l]
-    l and c should be given as strings and both be contained in Dataset ds
-    dl corresponds to the diffusive time tendency of tracer *content* of l,
-    i.e. the tendency of rho*l*h where h is layer thickness, and rho is in situ
-    density, thus in units of [kg m^-3]*[l tracer unit]*[m]*[s-1].
-    Then
-        E(l') = \frac{1}{\Delta l}\iint_{l=l'} c*dl dA
-    This uses the fact that the tracer content tendency is equal to the 
-    rho * tracer tendency integrated within a layer (which is formulation for
-    wmt in Groeskamp et al, 2019).'''
+def calc_P_wrapper(l,ps,l_l_vals,weight=None,dims=['xh','yh','zl'],greaterthan=True):
+    '''Wrapper for calc_P to allow integration of numerous quantities,
+    each represented as a DataArray in the Dataset [ps].
     
-    E = xr.Dataset()
+    *** IMPORTANT *** 
+    Limits of l_l_vals must span the full range of l.
+    *****************
     
-    # Calculate tracer at layer centres from interface values
-    l_l_vals = 0.5*(l_i_vals[:-1]+l_i_vals[1:])
-    # Calculate delta_tracer for each layer
-    delta_l_vals = np.diff(l_i_vals)
-    # Place in DataArrays
-    l_i=xr.DataArray(l_i_vals,dims=['l_i'],coords={'l_i':l_i_vals})
-    l_l=xr.DataArray(l_l_vals,dims=['l_l'],coords={'l_l':l_l_vals})
-    delta_l = xr.DataArray(delta_l_vals,dims=['l_l'],coords={'l_l':l_l_vals})
+    l : xr.DataArray;
+        Intensive variable (e.g. temp) that defines layers across which transport
+        will be determined.
+    ps : xr.Dataset;
+        Quantities for integration.
+        Multiple quantities are included as Dataarrays inside a dataset.
+    l_l_vals : ndarray or xr.DataArray
+        Values of the contours for which the integration will be calculated
+        *** This is currently set up such that the layer values are increasing ***
+        *** I expect it can be generalized, but have not explored this yet     ***
+    weight : xr.DataArray
+        Distribution of weights by which to multiply [p] to remove spatial
+        dimension of units. E.g. thickness in units 'm' should be multiplied by the area
+        of the grid cell to recover total volume.
+    greaterthan : boolean;
+        True if the integral should be over contours 
+        greater than each contour. False for less than.'''
+    
+    # Set up dataset for contributions to E
+    P = xr.Dataset()
+    for var in ps.data_vars:
+        p = ps[var]
+        P[var] = calc_volumetric_cumsum(l,p,l_l_vals,weight=weight,dims=dims,greaterthan=greaterthan)
+        
+    return P
 
-    # Calculate cdl
-    cdl = ds[c]*dl
-
-    # Bin into layers of l, and sum up across space
-    if binning is None:
-        work = (cdl*area).sum(dim=[xdim,ydim])
-        work = work.rename({vertc:'l_l'})
-    if binning=='xhistogram':
-        nanmask = np.isnan(cdl)
-        work = histogram(
-                    ds[l].where(~nanmask), 
-                    bins=[l_i.values], 
-                    dim=['xh','yh','zl','time'], 
-                    weights=(cdl*area).where(~nanmask),
-                    block_size=None
-                    )
-        work = work.rename({l+'_bin':'l_l'})
-    if binning=='busecke':
-        l_depth_i = vc.linear_interpolation_regrid(ds[z+'_l'], ds[l], l_i, z_bounds=ds[z+'_i'], target_value_dim='l_i', z_bounds_dim=zidim, z_dim=zldim)
-        cdl_remapped = conservative_remap(cdl,z_bnds_source=ds[z+'_i'], z_bnds_target=l_depth_i,
-                                           z_dim=zldim, z_bnd_dim=zidim, z_bnd_dim_target='regridded', mask=True)
-        work = (cdl_remapped*area).sum(dim=[xdim,ydim])
-        work = work.rename({'remapped':'l_l'})
-        E['l_depth_i'] = l_depth_i
+def calc_dPdt(l,p,l_l_vals,weight=None,greaterthan=True,dims=['xh','yh','zl'],dim_time='time',delta_t=None,new_time=None):
+    '''Take the volumetric cumulative sum of quantity [p] and calculate the change over time.
+    Within this function, the newly created dPdt can be *realigned* with other quantities, 
+    by defining a [new_time]. This is basically just a wrapper to simultaneously calculate
+    P, and its change over time.
     
-    E['E'] = work/delta_l
-    E['dE']= E['E'].interp(l_l=l_i,method='linear').diff('l_i').assign_coords(l_i=l_l.values).drop('l_l').rename({'l_i':'l_l'})
+    The most common use of this function will be to calculate the change in volume/mass/tracer
+    content over time.
     
-    return E
+    l : xr.DataArray;
+        Intensive variable, to define volume boundary
+    p : xr.DataArray;
+        Quantity to be integrated (e.g. thickness)
+    l_l_vals : ndarray or xr.DataArray
+        Values of the contours for which the integration will be calculated
+        *** This is currently set up such that the layer values are increasing ***
+        *** I expect it can be generalized, but have not explored this yet     ***
+    weight : Array of weights by which to multiply [p] to remove spatial
+        dimension of units. E.g. density units in units 'kgm-3' should be multiplied by the volume
+        of the grid cell to recover total mass.
+    greaterthan : boolean;
+        True if the integral should be over contours 
+        greater than each contour. False for less than.
+    dim_time : str
+        Name of time dimension in DataArrays
+    delta_t : xr.DataArray
+        Array of time differences between entries in [l] and [p]
+    new_time : xr.DataArray or np.array
+        Option to reassign time coordinates after differentiating, to align with other objects.'''
+    
+    if new_time is None:
+        new_time = l[dim_time]
+    
+    # If not defined, calculate the time from the loaded DataArrays
+    if delta_t is None:
+        delta_t_vals = l[dim_time].diff(dim_time).values.astype('timedelta64[s]').astype('int')
+        delta_t = xr.DataArray(delta_t_vals,dims=[dim_time],coords=({dim_time:new_time}))
+    
+    # Integrate the volume/mass/tracer
+    P = calc_volumetric_cumsum(l,p,l_l_vals,weight=weight,dims=dims,greaterthan=greaterthan)
+    # Calculate the change over time
+    dPdt = P.diff(dim_time).assign_coords({dim_time:new_time})/delta_t
+    
+    return dPdt
